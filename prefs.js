2eafd5d243e0a96d2323d2080c8947c7942dd344
Giorgio Dramis <dramisgiorgio@outlook.com>
2023-10-07 15:30:46 +0200

    extension preference page added

--- prefs.js
+++ prefs.js
@@ -1,5 +1,6 @@
 import Gio from 'gi://Gio';
 import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk';
 
 import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
@@ -14,21 +15,28 @@ export default class ExamplePreferences extends ExtensionPreferences {
         window.add(page);
 
         const group = new Adw.PreferencesGroup({
-            title: _('Appearance'),
-            description: _('Configure the appearance of the extension'),
+            title: _('Select default proxy'),
+            description: _('This is a concept and not wotking at all for now'),
         });
+        group.set_header_suffix(new Gtk.Button({
+            label: _('Add proxy'),
+            icon_name: 'list-add-symbolic',
+        }))
         page.add(group);
 
         // Create a new preferences row
-        const row = new Adw.SwitchRow({
-            title: _('Show Indicator'),
-            subtitle: _('Whether to show the panel indicator'),
+        const row1 = new Adw.SwitchRow({
+            title: _('Proxy two'),
+            subtitle: _('Select to enable the proxy'),
         });
-        group.add(row);
+        const row2 = new Adw.SwitchRow({
+            title: _('Proxy one'),
+            subtitle: _('Select to enable the proxy'),
+        });
+        group.add(row1);
+        group.add(row2);
 
         // Create a settings object and bind the row to the `show-indicator` key
         window._settings = this.getSettings();
-        window._settings.bind('feature-enabled', row, 'active',
-            Gio.SettingsBindFlags.DEFAULT);
     }
 }
